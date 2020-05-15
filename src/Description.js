5130a4333f77cb064ba85730ad55d4cc88be5e4d
anaknittel <62256247+anaknittel@users.noreply.github.com>
2020-05-15 14:20:18 +0100

    only icon is missing

--- src/Description.js
+++ src/Description.js
@@ -1,22 +1,27 @@
 import React from "react";
 
 export default function Description(props) {
-  if weather==null
-  return parag: searching;
-  else{
-  return (
-
-
-    
-    <div>
-
-      <ul>
-        <li>Temperature: 20ºC </li>
-        <li>Description: rainy </li>
-        <li>Humidity: 80% </li>
-        <li>Wind: 4m/s </li>
-        <li>Icon</li>
-      </ul>
-    </div>
-  );
+  if (props.weather == null) {
+    return <p> Please Search for a City </p>;
+  } else {
+    let icon = props.weather.weather[0].icon;
+    console.log(icon);
+    return (
+      <div>
+        <ul>
+          <li>Temperature: {props.weather.main.temp}ºC </li>
+          <li>Description: {props.weather.weather[0].description}</li>
+          <li>Humidity: {props.weather.main.humidity}% </li>
+          <li>Wind: {props.weather.wind.speed}m/s</li>
+          <li>
+            icon:{" "}
+            <img
+              src="http://openweathermap.org/img/wn/${icon}d@2x.png"
+              alt=""
+            />
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
